10034cc12896f23a731dfb8a48e43ccf6bae62a0
John Oenga Ongwae <johnongwae4@gmail.com>
2024-06-05 20:12:50 +0300

    Upgrade dependencies

--- src/test/java/io/github/oengajohn/departmentservice/controller/DepartmentControllerTest.java
+++ src/test/java/io/github/oengajohn/departmentservice/controller/DepartmentControllerTest.java
@@ -4,16 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
 import io.github.oengajohn.departmentservice.AbstractTestContainersTest;
 import io.github.oengajohn.departmentservice.model.DepartmentRequest;
 import io.github.oengajohn.departmentservice.model.DepartmentResponse;
--- src/test/java/io/github/oengajohn/departmentservice/repository/DepartmentRepositoryTest.java
+++ src/test/java/io/github/oengajohn/departmentservice/repository/DepartmentRepositoryTest.java
@@ -7,12 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
 import io.github.oengajohn.departmentservice.AbstractTestContainersTest;
 import io.github.oengajohn.departmentservice.entity.Department;
 
