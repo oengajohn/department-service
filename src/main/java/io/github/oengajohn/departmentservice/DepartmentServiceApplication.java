6a1c87f32cdf021104972f5176135ba90c4ae105
John Oenga Ongwae <johnongwae4@gmail.com>
2024-06-08 00:34:55 +0300

    Add actiator and devtools dependencies

--- src/main/java/io/github/oengajohn/departmentservice/DepartmentServiceApplication.java
+++ src/main/java/io/github/oengajohn/departmentservice/DepartmentServiceApplication.java
@@ -2,9 +2,10 @@ package io.github.oengajohn.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
